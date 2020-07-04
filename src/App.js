01dabd4f342993f5115e307f0aacdcf394ada426
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-07-04 21:22:49 +1000

    integrated contact component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Sidebar from './components/app/Sidebar/Sidebar';
 import Body from './components/app/Body/Body';
 
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+    }
+}));
+
 const App = () => {
+    const classes = useStyles();
+
     return (
         <div className="App">
-            <Sidebar />
-            <Body />
+            <Grid container className={classes.root}>
+                <Grid item xs={6} sm={5} md={4} lg={3}>
+                    <Sidebar />
+                </Grid>
+                <Grid item xs={6} sm={7} md={8} lg={9}>
+                    <Body />
+                </Grid>
+            </Grid>
         </div>
     );
 }
--- src/components/app/Sidebar/Sidebar.js
+++ src/components/app/Sidebar/Sidebar.js
@@ -1,8 +1,22 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core';
+import Contact from '../Contact';
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        backgroundColor: '#2e3f4f',
+    }
+}));
 
 const Sidebar = () => {
+    const classes = useStyles();
+
     return (
-        <p>Hello from sidebar :)</p>
+        <div className={classes.root}>
+            <Contact />
+        </div>
     );
 };
 
