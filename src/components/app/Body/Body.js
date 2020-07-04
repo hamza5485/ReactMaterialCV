af8596b24aee5cab3c6a8415be1a7d83007dfe22
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-07-04 19:46:32 +1000

    init

---
+++ src/components/app/Body/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Body = () => {
+    return (
+        <p>Hello from body :)</p>
+    );
+};
+
+export default Body;
---
+++ src/components/app/Sidebar/Sidebar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Sidebar = () => {
+    return (
+        <p>Hello from sidebar :)</p>
+    );
+};
+
+export default Sidebar;
