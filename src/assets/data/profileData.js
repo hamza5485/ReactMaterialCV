54f6ba497f6fc3c14bb9dfa9ecd07c854682fa81
Hamza Anas <hamza.anas@itinnovate.com.au>
2020-08-08 00:58:29 +1000

    small changes

--- src/assets/data/profileData.js
+++ src/assets/data/profileData.js
@@ -3,9 +3,9 @@ const profile = {
 	title: "Software Engineer",
 	summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras accumsan, enim vel posuere scelerisque, mauris odio viverra odio,nec egestas turpis magna vitae enim. Donec mauris est, vestibulum a tellus ac,mollis sagittis arcu. Nunc in sagittis erat. Nullam fermentum, diam et venenatis maximus,leo mi imperdiet lacus, vel sollicitudin turpis lacus id lectus. Donec ultrices laoreet efficitur.",
 	contact: {
-		website: { text: "hamzaanas.codes", link: "https://www.hamzaanas.dev" },
+		website: { text: "hamzaanas.codes", link: "https://www.hamzaanas.codes" },
 		email: { text: "hamza5485@gmail.com", link: "mailto:hamza5485@gmail.com" },
-		// number: { text: "(+61) 413 385 999", link: "tel:+61413385999" },
+		// number: { text: "NUMBER HERE", link: "tel:NUMBER_HERE" },
 		location: { text: "Sydney, Australia" }
 	},
 	social: {
